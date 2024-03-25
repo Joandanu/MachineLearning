2b84e216584c2380937c8ab96ddd2663432762bc
unknown <danuljoan@gmail.com>
2024-03-25 20:00:05 +0700

    MachineLearning

---
+++ Minggu 2/LatHitung.py
@@ -0,0 +1,42 @@
+#Jumlah
+print("Contoh Penjumlahan")
+Angka1 = float(input("Masukan Angka 1 =  "))
+Angka2 = float(input("Masukan Angka 2 =  "))
+Hasil = Angka1 + Angka2
+print("Hasil = ",Hasil)
+
+#Kurang
+print("Contoh Pengurangan")
+Angka1 = float(input("Masukan Angka 1 =  "))
+Angka2 = float(input("Masukan Angka 2 =  "))
+Hasil = Angka1 - Angka2
+print("Hasil = ",Hasil)
+
+#Kali
+print("Contoh Perkalian")
+Angka1 = float(input("Masukan Angka 1 =  "))
+Angka2 = float(input("Masukan Angka 2 =  "))
+Hasil = Angka1 * Angka2
+print("Hasil = ",Hasil)
+
+#Pembagian
+print("Contoh Pembagian")
+Angka1 = float(input("Masukan Angka 1 =  "))
+Angka2 = float(input("Masukan Angka 2 =  "))
+Hasil = Angka1 / Angka2
+print("Hasil = ",Hasil)
+
+#Menghitung Luas Persegi
+print("Contoh Perhitungan Luas Persegi")
+Panjang = float(input("Panjang  =  "))
+Lebar = float(input("Lebar    =  "))
+Luas = Panjang * Lebar
+print("Hasil = ",Luas)
+
+#Menghitung Luas Kubus
+print("Contoh Perhitungan Luas Kubus")
+sisi = 6
+Rusuk = float(input("Rusuk =  "))
+Luas = Rusuk * sisi
+print("Hasil = ",Luas)
+
